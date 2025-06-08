13eb82b8ef5a24cb871cdfb2caeabacc640b5bf6
suchit <sb.trizz@gmail.com>
2025-06-08 10:39:11 +0100

    fixed a typo

--- Core/Inc/i2c.h
+++ Core/Inc/i2c.h
@@ -9,7 +9,7 @@ extern I2C_HandleTypeDef hi2c1;
 void MX_I2C1_Init(void);
 void I2C_scan_devices(I2C_HandleTypeDef *hi2c, UART_HandleTypeDef *huart);
 void AHT20_soft_reset(void);
-void AHt20_Init(void);
+void AHT20_Init(void);
 void AHT20_trigger_measurement(void);
 void AHT20_get_measurement(uint8_t*);
 void AHT20_process_data(uint8_t raw_data[6], float *temperature_c, float *humidity_percent);
